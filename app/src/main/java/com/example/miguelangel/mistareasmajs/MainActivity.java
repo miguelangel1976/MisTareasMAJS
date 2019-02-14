d534add2f1e0877f848db4bbd7b8db1b71e9faf2
MIGUEL ANGEL <miguelindustriales2012@gmail.com>
2019-02-14 12:50:01 +0100

    Aplicacion Terminada a Falta del Icono

--- app/src/main/java/com/example/miguelangel/mistareasmajs/MainActivity.java
+++ app/src/main/java/com/example/miguelangel/mistareasmajs/MainActivity.java
@@ -1,18 +1,58 @@
 package com.example.miguelangel.mistareasmajs;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.miguelangel.mistareasmajs.db.ControladorDB;
+
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Para comunicar dos Clases "MainActivity.java" y "ControladorDB":
+    //Vamos a pasar una referencia al Controlador.
+    //Se crea el objeto de Forma Global, y se da la referencia al crear "onCreate" MainActivity
+    ControladorDB controladorDB;
+
+    //Crear un objeto "Adapter" que contenga un Array de Cadenas.
+//El ListView va a recibir el Array de Cadenas, con  que va a rellenar la Lista.
+
+    //Declaración del ArrayAdapter.
+    private ArrayAdapter<String> miAdaptadorCadenas;
+
+
+    //Tener una referencia al ListView
+
+    ListView listViewTareas;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Se pone this: este contexto: MainActivity
+        controladorDB = new ControladorDB(this);
+
+        //Hay que dar la Referencia al ListView
+        //Se hace un Casting porque devuelve un "View" y se necesita "ListView)
+        listViewTareas = (ListView)findViewById(R.id.listaTareas);
+
+        actualizarUI(); //Llamar al Método de "ACTUALIZAR INTERFAZ".
+
+
     }
 
     //Para Cargar la Barra
@@ -33,13 +73,104 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item){
 
-        AdapterView.AdapterContextMenuInfo info =(AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+        //AdapterView.AdapterContextMenuInfo info =(AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+
+        //Se crea un EditText dentro del Cuadro de Diálogo
+        //EditText necesita el contexto que es this (que esté dentro del MainActivity.java).
+
+        //Debido a ello, la definición del EditText se realiza fuera del "AlertDialog",
+        // porque desde éste no se puede acceder al contexto.
+
+        final EditText cajaTexto = new EditText(this);
+
+
 
       switch (item.getItemId()){
 
           case R.id.nuevaTarea:
               //Acciones: Visualizar Mensaje de Toast en la pantalla
-              Toast.makeText(this, "Añadir Tarea", Toast.LENGTH_SHORT).show();
+              //Toast.makeText(this, "Añadir Tarea", Toast.LENGTH_SHORT).show();
+
+
+              //Se ha sustituido del TOAST por el siguiente CUADRO DE DIÁLOGO
+
+              /*   */
+              //PRIMERA FORMA DE HACER EL "CUADRO DE DIÁLOGO":
+              //1. Instanciar el cuadro de diálogo con su constructor.
+
+              //Crear el "Cuadro de Diálogo"
+              AlertDialog cuadroDialogo = new AlertDialog.Builder(this)
+
+                //2. Se configuran los campos del "Cuadro de Diálogo"
+                .setTitle("Nueva Tarea.")
+                .setMessage("Introduce una Tarea que desees realizar.")
+                .setView(cajaTexto)//Visualizar Caja de Texto en el "Cuadro de Diálogo
+                .setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                        String tarea = cajaTexto.getText().toString();
+                        controladorDB.addTarea(tarea);
+
+
+
+                        actualizarUI(); //Llamar al Método de "ACTUALIZAR INTERFAZ".
+
+
+
+                        //Mensaje de Prueba Toast
+                        Toast.makeText(MainActivity.this, "Se ha añadido una nueva tarea.", Toast.LENGTH_SHORT).show();
+                    }//Fin del onClick
+                })//Fin del setPositiveButton
+
+                //No se va hacer nada al pulsar el botón cancelar
+                .setNegativeButton("Cancelar", null)
+
+                //3.Crear Cuadro de Diálogo.
+                .create();
+
+                //4. Mostar el Cuadro de Diálogo en Pantalla
+                cuadroDialogo.show();
+
+                //Fin de la PRIMERA FORMA DE HACER EL "CUADRO DE DIÁLOGO"
+                /*   */
+
+
+
+              /*
+              //SEGUNDA FORMA DE HACER EL "CUADRO DE DIÁLOGO":
+              //1. Instanciar el cuadro de diálogo con su constructor.
+
+              //Crear el "Cuadro de Diálogo"
+              AlertDialog.Builder cuadroDialogo = new AlertDialog.Builder(this);
+
+              //2. Se configuran los campos del "Cuadro de Diálogo"
+              cuadroDialogo.setTitle("Nueva Tarea.");
+              cuadroDialogo.setMessage("Introduce una Tarea que desees realizar.");
+              cuadroDialogo.setView(cajaTexto);//Visualizar Caja de Texto en el "Cuadro de Diálogo
+              cuadroDialogo.setPositiveButton("Añadir", new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialogInterface, int i) {
+                      //Mensaje de Prueba Toast
+                      Toast.makeText(MainActivity.this, "Se ha añadido una nueva tarea.", Toast.LENGTH_SHORT).show();
+
+                   }//Fin del onClick
+              });//Fin del setPositiveButton
+
+              //No se va hacer nada al pulsar el botón cancelar
+              cuadroDialogo.setNegativeButton("Cancelar", null);
+
+              //3.Crear Cuadro de Diálogo.
+              cuadroDialogo.create();
+
+              //4. Mostar el Cuadro de Diálogo en Pantalla
+              cuadroDialogo.show();
+
+              //Fin de la SEGUNDA FORMA DE HACER EL "CUADRO DE DIÁLOGO"
+              */
+
+
+
               return true;
 
           default:
@@ -49,5 +180,123 @@ public class MainActivity extends AppCompatActivity {
 
 
     } //Fin del Método onOptionsItemSelected
+
+
+
+    //Método que permite actualizar el interfaz usuario con la Base
+    //de Datos.
+    //Va a ser un Método que unicamente va a ser llamado desde esta
+    //clase, por lo que puede ser "Privado (private)".
+
+    //Se va a llamar a este Método "actualizarUI" en:
+    // Al crear "onCreate" de MainActivity: "LA PRIMERA VEZ QUE SE CREA EL ACTIVITY".
+    // Al agregar una nueva tarea, se debe también actualizar: "onClick" de MainActivity.
+    // En el Método "borrarTarea" de la clase "MainActivity": cuando se borra un dato de la Base de Datos.
+
+    private void actualizarUI(){
+        //Para actualizar el ListView, para que coja elementos
+        // se necesita un objeto "Adapter" que contenga un array de caracteres.
+
+        // Poniendo una cadena en cada uno de los elementos del ListView,
+        //cada vez que se utilize un ListView se necesita un ArrayAdapter.
+
+        //Crear objeto de ArrayAdapter
+        //This: este contexto; R.layout.item_tarea:para indicar con qué elementos vamos a rellenar el ListView.
+        //R.id.titulo_tarea: Para indicar lo que necesitamos dentro del "item_tarea.
+        //Para indicar con qué lo vamos a rellenar: Con los registros de la Base de Datos:
+        //Por lo que hay que llamar al ControladorDB que tiene el método "obtenerTareas".
+
+
+
+        //SI ES LA PRIMERA VEZ QUE ENTRAMOS VAMOS A OBTENER UN NULO, y en el método obtenerTareas VAMOS A TENER PROBLEMAS.
+        //ANTES DE ACTUALIZAR EL LIST VIEW ES NECESARIO CONOCER CUANTOS REGISTROS TENEMOS EN LA BASE DE DATOS.
+
+
+        //SI NO HAY ELEMENTOS HAY QUE PASAR AL setAdapter un NULL.
+        //listViewTareas.setAdapter(null);
+
+        //PARA CONOCER EL NÚMERO DE REGISTROS QUE TIENE LA TABLA SE CREA UN Método "numeroRegistros"
+        // en la Clase "ControladorDB".
+
+        int valor_numero_registros = controladorDB.numeroRegistros();
+
+        if(valor_numero_registros == 0){
+            //Si no hay registros en la Base de Datos: se da al ListView un "setAdapter" nulo.
+            listViewTareas.setAdapter(null);
+
+        }//Fin del If
+
+        else{
+
+            //SI HAY REGISTROS SE HACE LO SIGUIENTE:
+            miAdaptadorCadenas=new ArrayAdapter<>(this,R.layout.item_tarea,R.id.titulo_tarea,controladorDB.obtenerTareas());
+
+            ;
+
+            //Asignar al "ListView" el "Adaptador De cadenas".
+            listViewTareas.setAdapter(miAdaptadorCadenas);
+
+        }//Fin del Else
+
+
+    }//Fin del Método "actualizarUI"
+
+
+
+
+
+    //Método asociado al Botón "Borrar Tarea".
+    //El Botón "Borrar Tarea" llama a este método.
+
+    public void borrarTarea(View view){
+    //Se va a llamar a un método del Controlador que se encargue de dar de baja.
+    //Se va a dar el texto de la tarea que se desea dar de baja en la Base de Datos.
+
+
+         //En el "item_tarea.xml":
+        //Hay que coger el texto del TextView asociado al Botón "Borrar Tarea" que se ha pulsado (CLICK).
+        // Para ello, coger el Botón.  Del Botón se obtiene el Padre que en nuestro caso es "RelativeLayout"
+        //y desde el Padre se busca un elemento con el identificador del TextView android:id="@+id/titulo_tarea".
+        //De esta forma nos va a dar el TextView que acompaña a ese botón, y desde aquí obtener el String del TextView.
+
+        //Si no se hace lo anterior, al pulsar un determinado botón "Borrar Tarea" me empieza a borrar los primeros elementos
+        //de la lista o ListView.
+
+
+        //1. Obtener el Padre del Botón.
+        //View es el Botón, debido a que el Botón es el que llama a este Método Actual:"borrarTarea"
+        //View es el componente que ha llamado al Método: El Botón.
+
+        View parent = (View)view.getParent(); //getParent retorna un ViewParent, hay que hacer el casting a View.
+            //El "parent" es el "RelativeLayout", y desde aquí se busca el TextView con id=titulo_tarea.
+
+        //2. Dentro del Padre buscamos el identificador del TextView que está asociado al Botón.
+
+        //Devuelve un View, por lo que se necesita hacer casting a TextView
+        TextView tareaTextView =(TextView)parent.findViewById(R.id.titulo_tarea);
+
+        //tareaTextView es la Caja de Texto.
+
+
+        //3.Obtener el contenido del TextView
+            String nombreTarea = tareaTextView.getText().toString();
+
+
+       //El texto del TextView asociado al Botón que se ha pulsado, se envia al Método "BorrarTarea"
+       //de la clase "controladorDB".
+        controladorDB.borrarTarea(nombreTarea);
+
+
+        //4. Actualizar la interfaz una vez eliminado los datos en la Base de Datos.
+
+        actualizarUI(); //Llamar al Método de "ACTUALIZAR INTERFAZ".
+
+        //Mensaje de Prueba Toast
+        Toast.makeText(MainActivity.this, "Se ha Eliminado la tarea indicada.", Toast.LENGTH_SHORT).show();
+
+
+    }//Fin del Método "borrarTarea"
+
+
 
 }//Fin de la Clase MainActivity
---
+++ app/src/main/java/com/example/miguelangel/mistareasmajs/db/ControladorDB.java
@@ -0,0 +1,247 @@
+package com.example.miguelangel.mistareasmajs.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/*public class ControladorDB extends SQLiteOpenHelper {
+    public ControladorDB(Context context,  String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+*/
+/*Hay que pasarle:
+- El nombre de la Base de Datos: que es el nombre del paquete.
+
+
+
+ */
+public class ControladorDB extends SQLiteOpenHelper {
+
+
+    //CONDICIONES Y PASOS A SEGUIR:
+    //1.El controlador extienda sea hija de SQLiteOpenHelper (ControladorDB extends SQLiteOpenHelper).
+    //2. Necesita un Controlador para indicar donde se va a crear la Base de Datos y la Versión.
+    //3. Con el Método onCreate se crea la Tabla.
+
+    //Constructor.
+    public ControladorDB(Context context) {
+        super(context, "com.example.miguelangel.mistareasmajs.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase baseDatos) {
+        //Es donde se va a crear la Tabla de la Base de Datos
+        //y un método "Constructor" que también es obligatorio
+
+        //En este método se pueden ejecutar instrucciones SQL
+        //Para ejecutar una instrucción SQL se utiliza el método: execSQL
+
+
+        //Primer Parámetro: ID entero autoincremental y Primary Key (PK).
+        //Segundo Parámetro: Nombre que va a ser un Texto no Nulo (Dato Obligatorio).
+
+        //PRIMERA FORMA
+       /* String consulta = "CREATE TABLE TAREAS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL)";
+        baseDatos.execSQL(consulta);
+        */
+
+       //SEGUNDA FORMA
+        baseDatos.execSQL("CREATE TABLE TAREAS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NOMBRE TEXT NOT NULL)");
+
+    }
+
+    //Este Método sirve para migración de datos cuando se cambia la
+    //Versión de la Base de Datos.
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }//Fin del Método onUpgrade
+
+
+    //El Método "addTarea" va a ser pública, y tiene como parámetro el nombre de la tarea.
+    public void addTarea(String tarea){
+    /*Todos los métodos que vamos a poner aquí: Insertar, Consultar, Actualizar y Borrar
+    se definen en tres pasos:
+         1)Abrir la Base de Datos.
+         2)Sentencias: Ejecutar la Acción correspondiente.
+         3)Cerrar la Base de Datos.
+    */
+
+    /*Para abrir la Base de Datos necesitamos el Objeto del Controlador
+    y obtener la Base de Datos: Para Insertar : Se abre la Base de Datos para Lectura y Escritura.
+    que devuelve una referencia a la base de datos, almacenandose en una variable "db"
+    de Clase SQLiteDatabase.
+     */
+
+    //1. Se está abriendo la Base de Datos para Lectura y Escritura.
+    SQLiteDatabase db=this.getWritableDatabase();
+
+    //2. Lo siguiente es realizar la operación de INSERTAR.
+    // El registro que vamos a insertar debe ser del tipo ContentValues.
+    //El ContentValues es para poner la clave (Nombre del Campo) y Valor asignado
+
+        ContentValues registro = new ContentValues();
+        registro.put("NOMBRE",tarea); //Nombre del Campo: "NOMBRE", y valor asignado: "TAREA".
+
+        //Nombre de la Tabla: "TAREAS", Se introduce el registro.
+
+        //Primera Forma de hacerlo:
+        db.insert("TAREAS",null,registro);
+
+        //Segunda Forma de hacerlo:
+       // db.execSQL("INSERT INTO TAREAS VALUES(null, ' + tarea + ');");
+
+     //3. Cerrar la Base de Datos.
+     db.close();
+
+
+    }//Fin del Método addTarea
+
+
+    //Método Público para acceder desde otra clase, y devuelve un array de cadenas: String[].
+    public String[] obtenerTareas(){
+
+// 1)Abrir la Base de Datos.Para consulta se puede hacer "Solo Lectura" con el método getReadableDatabase().
+        SQLiteDatabase db=this.getReadableDatabase();
+
+        // 2)Sentencias: Ejecutar la Acción correspondiente.
+        //Para realizar consultas se realiza con el método: rawQuery(), en el que se le pasa la consulta en SQL.
+        //También es necesario indicar los parámetros que van con la sentencia SQL mediante ?:
+        //debido a que no estamos utilizando ningún parámetro "?"en la sentencia SQL: se pone NULL.
+
+        //Se obtiene campo 0 al Id y campo 1 al nombre.
+        Cursor cursor=db.rawQuery("SELECT * FROM TAREAS", null);
+
+
+        //Se obtiene campo 0 al nombre.
+        //Cursor cursor=db.rawQuery("SELECT NOMBRE FROM TAREAS", null);
+
+
+        //Al hacer esta consulta, se obtiene un conjunto de Registros: denominado "Cursor",
+        //consiste a una referencia de los registros que he sacado con el SELECT.
+
+
+        //Hay que recorrer el "cursor" que es un conjunto de Registros.
+
+        //El número de registros del "cursor" se obtiene mediante:getCount().
+
+        int numero_registros = cursor.getCount();
+
+        //Si el "numero_registros" es igual a 0, significa que no hay elementos en la Tabla.
+
+        if(numero_registros == 0){ //Tabla Vacia
+            db.close();//3)Cerrar la Base de Datos.
+            return null; //No hay nada que retornar.
+
+        }//Fin del If
+
+
+        else{ //Hay Elementos en la Tabla, por lo que hay registros en el Cursor.
+            //Se procede a recorrer los registros del cursor, con el propósito de
+            //almacenarlos en un array String y retornar dichor array.
+
+            //Mover al Principio del Cursor.
+            cursor.moveToFirst(); //Ir al Primer registro del cursor.
+
+            //Declarar array de "tareas" con tamaño igual al de numero_registros.
+            String[]tareas = new String[numero_registros];
+
+
+            //Recorrer el Cursor
+            for(int i=0; i<numero_registros;i++){
+                //El campo 0 de la Tabla corresponde al "ID"
+                //El campo 1 de la Tabla corresponde al "Nombre"
+                //Nos interesa el Campo 1, debido a que el campo 0 es autonumérico.
+
+                tareas[i]=cursor.getString(1);
+                //Se coge el Primer Campo debido a que se ha hecho un "SELECT * FROM TAREAS
+                //por lo que se obtiene el campo 0 correspondiente al "ID" y el campo 1 al "Nombre".
+
+                //Si se hubiera hecho la sentencia "SELECT NOMBRE FROM TAREAS
+                //La tabla solo obtiene un único campo que es nombre, y habría que haber
+                //escogido el campo "0"
+                //tareas[i]=cursor.getString(0);
+
+
+                //Mover el cursor al siguiente Registro
+                cursor.moveToNext();
+
+            }//Fin del for
+
+            //3)Una vez almacenados los registros en el array, cerrar la base de datos.
+            db.close();
+
+            return tareas; //Retornar el Array
+
+        }//Fin del Else
+
+
+    }//Fin del Método obtenerTareas
+
+
+
+
+    //Método que devuelve el Número de Registros que Tiene la Tabla
+    //Para conocer si la TABLA TIENE REGISTROS para el Método "ActualizarUI" de la Clase "MainActivity"
+    public int numeroRegistros(){
+
+        //1)Abrir la base de Lecturas "En Modo Lectura" para Consultas.
+        SQLiteDatabase db=this.getReadableDatabase();
+        //Se obtiene campo 0 al Id y campo 1 al nombre.
+        Cursor cursor=db.rawQuery("SELECT * FROM TAREAS", null);
+
+        //2)Obtener número Registros de la Tabla.
+        int numero_registros = cursor.getCount();
+
+        //3)Cerrar La Base de Datos.
+        db.close();
+
+        //Retornar valor del número registros de la Tabla.
+        return numero_registros;
+    }//Fin del Método numeroRegistros
+
+
+
+
+
+    //Método para dar de baja de la Base de Datos una determinada Tarea
+
+    //Debe ser pública porque se va acceder desde otras clases.
+    //No retorna ningún valor.
+    //Recibe como parámetro el nombre de la tarea a Borrar.
+
+    //Este métood es llamado desde el Método "borrarTarea" del MainActivity
+    public void borrarTarea(String tarea){
+       // 1)Abrir la Base de Datos en modo Lectura y Escritura, debido a que se van a Borrar Datos.
+        SQLiteDatabase db=this.getWritableDatabase();
+
+
+       //2)Sentencias: Ejecutar la Acción correspondiente.
+        //Al poner el símbolo de interrogación se dice que es un parámetro del DELETE,
+        // que va en el siguiente argumento.
+        //Los simbolos de interrogación es un array de parámetros, ya que va a ser
+        //una Consulta Parametrizada.
+
+        //En nuestro caso el parámetro es el nombre de la Tarea que se desea Borrar.
+        //Requiere que pasemos un Array de String y no un únimo String que es "tarea".
+
+        //Por lo que hay que convertir el String "tarea" en un Array String de un único elemento.
+
+        // Pasar: String tarea;  -->  nes String[]{tarea};
+        db.delete("TAREAS","NOMBRE=?", new String[]{tarea});
+
+
+       // 3)Cerrar la Base de Datos.
+        db.close();
+    }//Fin del Método borrarTarea
+
+
+
+
+
+
+
+}//Fin de la Clase
